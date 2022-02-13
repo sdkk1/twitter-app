e0f477ab7e2af60c1f70d857da84d5bd1db18923
Keita <fast.break.baske03@gmail.com>
2022-02-13 21:59:37 +0900

    add: auth ページコンポーネントを作成

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,33 @@
+import { CssBaseline, Paper, Grid, makeStyles } from '@material-ui/core'
+
+import AuthFrom from 'src/components/pages/auth/AuthFrom'
+
+export default function Auth() {
+  const classes = useStyles()
+
+  return (
+    <Grid container component='main' className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <AuthFrom />
+      </Grid>
+    </Grid>
+  )
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+}))
