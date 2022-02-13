968c5ccadf4951960a521765b662bd81a40d5bd9
Keita <fast.break.baske03@gmail.com>
2022-02-13 22:02:20 +0900

    update: ログインの有無でページを切り替える処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
-import './App.css'
+/** @jsxImportSource @emotion/react */
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { css } from '@emotion/react'
 
-function App() {
+import Feed from 'src/pages/feed'
+import Auth from 'src/pages/auth'
+import { selectUser, login, logout } from 'src/features/userSlice'
+import { auth } from 'src/firebase'
+
+export default function App() {
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        )
+      } else {
+        dispatch(logout())
+      }
+    })
+
+    return () => {
+      unSub()
+    }
+  }, [dispatch])
+
   return (
-    <div className='App'>
-      <h1>Create React App</h1>
-    </div>
+    <>
+      {user.uid ? (
+        <div css={styleApp}>
+          <Feed />
+        </div>
+      ) : (
+        <Auth />
+      )}
+    </>
   )
 }
 
-export default App
+const styleApp = () => css`
+  display: flex;
+  height: 100vh;
+  padding: 30px 80px;
+  background-color: #444447;
+`
