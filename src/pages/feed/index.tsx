611c500a3c52968d0a15aad0443de1cd975d2511
Keita <fast.break.baske03@gmail.com>
2022-02-19 14:45:29 +0900

    add: feed ページコンポーネントを作成

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -1,10 +1,61 @@
-import { auth } from 'src/firebase'
+/** @jsxImportSource @emotion/react */
+import React, { useState, useEffect } from 'react'
+import { css } from '@emotion/react'
+
+import FeedTweetForm from 'src/components/pages/feed/FeedTweetForm'
+import FeedTweetList from 'src/components/pages/feed/FeedTweetList'
+import { db } from 'src/firebase'
+import { tweetInfo } from 'src/types/tweet'
 
 export default function Feed() {
+  const [tweets, setTweets] = useState<tweetInfo[]>([])
+
+  useEffect(() => {
+    const unSub = db
+      .collection('posts')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot(snapshot => {
+        setTweets(
+          snapshot.docs.map(doc => {
+            return {
+              id: doc.id,
+              avatar: doc.data().avatar as string,
+              image: doc.data().image as string,
+              text: doc.data().text as string,
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+              timestamp: doc.data().timestamp,
+              userName: doc.data().userName as string,
+            }
+          })
+        )
+      })
+
+    return () => {
+      unSub()
+    }
+  }, [])
+
   return (
-    <>
-      <div>Feed</div>
-      <button onClick={() => auth.signOut()}>Logout</button>
-    </>
+    <div css={styleFeed}>
+      <FeedTweetForm />
+      {tweets.length > 0 &&
+        tweets.map(tweet => (
+          <FeedTweetList
+            key={tweet.id}
+            tweetId={tweet.id}
+            avatar={tweet.avatar}
+            image={tweet.image}
+            text={tweet.text}
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            timestamp={tweet.timestamp}
+            userName={tweet.userName}
+          />
+        ))}
+    </div>
   )
 }
+
+const styleFeed = () => css`
+  flex: 1;
+  overflow-y: scroll;
+`
